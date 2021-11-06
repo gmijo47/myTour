e7a6321103f65ad08b58cd80b883a1349c58d31f
Galić Mijo <gmijo47@gmail.com>
2021-11-06 16:10:04 +0100

    Formatting

--- app/src/main/java/com/gmijo/mytour/Login.java
+++ app/src/main/java/com/gmijo/mytour/Login.java
@@ -8,7 +8,6 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.os.Handler;
-import android.util.Log;
 import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
@@ -122,9 +121,13 @@ public class Login extends AppCompatActivity  {
                     @Override
                     public void onComplete(@NonNull Task<Void> task) {
                         if (task.isSuccessful()){
+
                             setError("succVerifySent", 10000);
+
                         }else {
+
                             setError("errVerifyNotSent", 10000);
+
                         }
                     }
                 });
