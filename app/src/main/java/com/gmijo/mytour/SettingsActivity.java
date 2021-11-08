252d5ef4d49b324574be650b42f572c8cd480b51
Galić Mijo <gmijo47@gmail.com>
2021-11-08 19:59:26 +0100

    Novi activity (postavke)

---
+++ app/src/main/java/com/gmijo/mytour/SettingsActivity.java
@@ -0,0 +1,48 @@
+package com.gmijo.mytour;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.PreferenceFragmentCompat;
+
+import com.gmijo.mytour.ui.profil.ProfilFragment;
+
+public class SettingsActivity extends AppCompatActivity {
+    ImageButton settingsBackBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.settings_activity);
+
+        settingsBackBtn = (ImageButton) findViewById(R.id.settingsBack);
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.settings, new SettingsFragment())
+                    .commit();
+        }
+        ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+        }
+        settingsBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+    }
+
+    public static class SettingsFragment extends PreferenceFragmentCompat {
+        @Override
+        public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
+            setPreferencesFromResource(R.xml.root_preferences, rootKey);
+        }
+    }
+}
