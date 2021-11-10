c61325307ca6a57bbb5cedf7e098a8aeeea9cd26
Galić Mijo <gmijo47@gmail.com>
2021-11-10 21:55:24 +0100

    Dodat novi activity za edit profila

---
+++ app/src/main/java/com/gmijo/mytour/EditProfile.java
@@ -0,0 +1,44 @@
+package com.gmijo.mytour;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageButton;
+import android.widget.ImageView;
+
+public class EditProfile extends AppCompatActivity {
+
+    //Inicijaliziranje i definisanje elemenata
+    ImageButton editProfileBackBtn, editProfileSaveBtn;
+    ImageView editAvatar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_profile);
+
+        editProfileBackBtn = (ImageButton) findViewById(R.id.editProfileBack);
+        editProfileSaveBtn = (ImageButton) findViewById(R.id.editProfileSave);
+        editAvatar = (ImageView) findViewById(R.id.editAvatar);
+
+        editProfileBackBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Isti dialog za save
+            }
+        });
+        editAvatar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+
+    @Override
+    public void onBackPressed() {
+        super.onBackPressed();
+        //Dialog bez save
+    }
+}
