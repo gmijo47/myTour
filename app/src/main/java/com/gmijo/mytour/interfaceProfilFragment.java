68190997afa9f3e00bc8733b351d883d5c349538
Galić Mijo <gmijo47@gmail.com>
2021-11-30 11:54:10 +0100

    Dodat interface, za interakciju između fragmenta i activitja

---
+++ app/src/main/java/com/gmijo/mytour/interfaceProfilFragment.java
@@ -0,0 +1,10 @@
+package com.gmijo.mytour;
+
+import java.util.ArrayList;
+
+public interface interfaceProfilFragment {
+    void displayData(ArrayList<String> data);
+
+    void setError(String errFailedToGetData);
+
+}
