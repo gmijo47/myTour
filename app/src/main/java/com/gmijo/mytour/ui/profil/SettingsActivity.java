c025aa15d3f8efcb121cb95defc9186649c4e805
Galić Mijo <gmijo47@gmail.com>
2021-12-13 14:54:07 +0100

    Dodato brisanje korisničkog naloga

--- app/src/main/java/com/gmijo/mytour/ui/profil/SettingsActivity.java
+++ app/src/main/java/com/gmijo/mytour/ui/profil/SettingsActivity.java
@@ -5,14 +5,22 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.preference.Preference;
 import androidx.preference.PreferenceFragmentCompat;
 
+import com.gmijo.mytour.Login;
 import com.gmijo.mytour.R;
-import com.gmijo.mytour.ui.profil.ProfilFragment;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
 
-public class SettingsActivity extends AppCompatActivity {
+public class SettingsActivity extends AppCompatActivity{
     ImageButton settingsBackBtn;
 
     @Override
@@ -20,6 +28,7 @@ public class SettingsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.settings_activity);
 
+        //Dobavljanje buttona
         settingsBackBtn = (ImageButton) findViewById(R.id.settingsBack);
 
         if (savedInstanceState == null) {
@@ -41,9 +50,61 @@ public class SettingsActivity extends AppCompatActivity {
     }
 
     public static class SettingsFragment extends PreferenceFragmentCompat {
+
+        //Inicijalizacija elemnata
+        Preference delProfile;
+        FirebaseFirestore firebaseFirestore;
+        FirebaseUser firebaseUser;
+        FirebaseAuth firebaseAuth;
+
+
         @Override
         public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
             setPreferencesFromResource(R.xml.root_preferences, rootKey);
+
+        }
+
+        @Override
+        public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+            super.onViewCreated(view, savedInstanceState);
+
+            //Dobavljanje firebase-a
+            firebaseAuth = FirebaseAuth.getInstance();
+            firebaseUser = firebaseAuth.getCurrentUser();
+            firebaseFirestore = FirebaseFirestore.getInstance();
+
+            //Dobavljanje preference, i lisener na istu
+            delProfile = (Preference) findPreference("delProfile");
+            delProfile.setOnPreferenceClickListener(new Preference.OnPreferenceClickListener() {
+                @Override
+                public boolean onPreferenceClick(Preference preference) {
+
+                    //Brisanje podataka u bazi
+                    firebaseFirestore.collection("users").document(firebaseUser.getUid()).delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                        @Override
+                        public void onComplete(@NonNull Task<Void> task) {
+                            if (task.isSuccessful()){
+
+                                //Brisanje samog usera
+                                firebaseUser.delete().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if (task.isSuccessful()){
+
+                                            //Kreiranje novog intenta, i završavanje svih activija, i statovanje logina
+                                            Intent rs = new Intent(getActivity(), Login.class);
+                                            getActivity().finishAffinity();
+                                            startActivity(rs);
+                                        }
+                                    }
+                                });
+                            }
+                        }
+                    });
+
+                    return false;
+                }
+            });
         }
     }
 }
