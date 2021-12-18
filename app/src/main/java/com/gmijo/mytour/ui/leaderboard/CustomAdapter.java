8d44f4c2bc17bc97f4a43deb9d32dbbe0c380c4a
Galić Mijo <gmijo47@gmail.com>
2021-12-18 22:56:16 +0100

    Sitne izmjene, za dobavljanje elemenata

--- app/src/main/java/com/gmijo/mytour/ui/leaderboard/CustomAdapter.java
+++ app/src/main/java/com/gmijo/mytour/ui/leaderboard/CustomAdapter.java
@@ -17,12 +17,10 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     //Inicijalizacija elemenata
     ArrayList<String> username_list, count_value_list;
-    Context context;
-    CustomAdapter customAdapter;
+
 
     //Prosljeđivanje elemenata klasi iznad
-    public CustomAdapter(Context ctx, ArrayList<String> username_list, ArrayList<String> count_value_list){
-        this.context = context;
+    public CustomAdapter(ArrayList<String> username_list, ArrayList<String> count_value_list){
         this.username_list = username_list;
         this.count_value_list = count_value_list;
     }
@@ -30,15 +28,15 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
     @NonNull
     @Override
     public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        LayoutInflater inflater = LayoutInflater.from(context);
-        View view = inflater.inflate(R.layout.adapter_leaderboard_layout, parent, false );
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        View view = inflater.inflate(R.layout.adapter_leaderboard_layout, parent, false);
 
         return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
-        holder.id.setText(String.valueOf(position));
+        holder.id.setText(String.valueOf(position+1));
         holder.username.setText(String.valueOf(username_list.get(position)));
         holder.count.setText(String.valueOf(count_value_list.get(position)));
 
@@ -46,7 +44,7 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
 
     @Override
     public int getItemCount() {
-        return 0;
+        return username_list.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
@@ -58,9 +56,9 @@ public class CustomAdapter extends RecyclerView.Adapter<CustomAdapter.ViewHolder
             super(itemView);
 
             //Dobavljanje elemenata
-            id = itemView.findViewById(R.id.lbNOUser);
-            username = itemView.findViewById(R.id.lbUsername);
-            count = itemView.findViewById(R.id.lbCount);
+            id = (TextView) itemView.findViewById(R.id.lbNOUser);
+            username = (TextView) itemView.findViewById(R.id.lbUsername);
+            count = (TextView) itemView.findViewById(R.id.lbCount);
 
         }
     }
