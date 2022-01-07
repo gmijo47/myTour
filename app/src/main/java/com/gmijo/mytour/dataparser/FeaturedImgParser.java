1fddd0ac78d9ad6201ba5918ca7822444a2d184c
Galić Mijo <gmijo47@gmail.com>
2022-01-07 18:55:45 +0100

    Dodat parser za slike, i activity za info

---
+++ app/src/main/java/com/gmijo/mytour/dataparser/FeaturedImgParser.java
@@ -0,0 +1,52 @@
+package com.gmijo.mytour.dataparser;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+
+public class FeaturedImgParser extends AsyncTask<String, Void, Void> {
+
+    //Interface za async task
+    public interface AsyncResponse {
+        void processFinish(String output);
+    }
+
+    public AsyncResponse delegate = null;
+
+    public FeaturedImgParser(AsyncResponse delegate){
+        this.delegate = delegate;
+    }
+
+    @Override
+    protected Void doInBackground(String... rawUrl) {
+        try {
+
+            //Otvara url
+            Document doc = Jsoup.connect(rawUrl[0]).get();
+
+            //Parsovanje html elemenata
+            Element htmlElement = doc.select("img").first();
+            String hElement = htmlElement.toString();
+
+           //Selectovanje i filtriranje urla
+           int startIndex = hElement.indexOf("src=\"");
+           int endIndex = hElement.indexOf("\" alt=", startIndex);
+           String imageUrl = hElement.substring(startIndex + 5, endIndex);
+
+
+           //Vraća url
+           delegate.processFinish(imageUrl);
+
+
+        }catch (Exception e){
+            Log.e("err", String.valueOf(e));
+            e.printStackTrace();
+
+
+        }
+        return null;
+    }
+}
---
+++ app/src/main/java/com/gmijo/mytour/ui/pocetna/PlaceInfoActivity.java
@@ -0,0 +1,22 @@
+package com.gmijo.mytour.ui.pocetna;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+import com.gmijo.mytour.R;
+
+
+public class PlaceInfoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_place_info);
+
+
+
+
+    }
+}
