fa75331ed8517b862f0e9e1c19ebca767f1bf5d4
Galić Mijo <gmijo47@gmail.com>
2022-01-09 22:43:04 +0100

    Dodate featured atrakcije na pocetnu

--- app/src/main/java/com/gmijo/mytour/ui/pocetna/PocetnaFragment.java
+++ app/src/main/java/com/gmijo/mytour/ui/pocetna/PocetnaFragment.java
@@ -17,9 +17,12 @@ import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.gmijo.mytour.R;
+import com.gmijo.mytour.database.SQLiteAttractionDataHelper;
 import com.gmijo.mytour.database.SQLiteCityDataHelper;
 
+import static com.gmijo.mytour.database.SQLiteAttractionController.*;
 import static com.gmijo.mytour.database.SQLiteCityController.*;
+
 
 import java.io.FileOutputStream;
 import java.io.IOException;
@@ -31,16 +34,21 @@ import java.util.List;
 public class PocetnaFragment extends Fragment {
 
     View view;
-    RecyclerView cityFeatured, natureFeatured;
-    FeaturedAdapter featuredAdapter;
+    RecyclerView cityFeatured, attractionFeatured;
+    FeaturedAttractionAdapter attractionAdapter;
+    FeaturedCityAdapter featuredCityAdapter;
     List<Pair<Pair<String, String>, Pair<String, Pair<String, String>>>> dataCity = new ArrayList<>();
+    List<Pair<Pair<String, String>, Pair<String, Pair<String, String>>>> dataAttraction = new ArrayList<>();
     SQLiteCityDataHelper liteCityDataHelper;
+    SQLiteAttractionDataHelper liteAttractionDataHelper;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
          view = inflater.inflate(R.layout.fragment_pocetna, container, false);
         liteCityDataHelper = new SQLiteCityDataHelper(getActivity());
+        liteAttractionDataHelper = new SQLiteAttractionDataHelper(getActivity());
         cityFeatured = (RecyclerView) view.findViewById(R.id.fCityRecyclerView);
+        attractionFeatured = (RecyclerView) view.findViewById(R.id.fAttractionRecyclerView);
          return  view;
     }
 
@@ -52,37 +60,28 @@ public class PocetnaFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        new Handler().postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                getActivity().findViewById(R.id.loaderBox).setVisibility(View.GONE);
-                getActivity().findViewById(R.id.fragmentProgBar).setVisibility(View.VISIBLE);
-                getActivity().findViewById(R.id.errImg).setVisibility(View.GONE);
-                getActivity().findViewById(R.id.loaderError).setVisibility(View.GONE);
-            }
-        }, 2000);
 
        //Kopiranje baze iz assetsa, u /data/data/com.gmijo.mytour/databases
        try{
 
            //Dobavljanje IO streamova
-           InputStream openRawResource = getActivity().getAssets().open("raw.db");
-           FileOutputStream fileOutputStream = new FileOutputStream(getActivity().getDatabasePath(DATABASE_NAME));
+           InputStream openRawCityResource = getActivity().getAssets().open("raw.db");
+           FileOutputStream fileCityOutputStream = new FileOutputStream(getActivity().getDatabasePath(CITY_DATABASE_NAME));
 
            //Kopira po 1024 byta istovremeno, loopuje sve dok ne prođe kroz sve
            byte[] bArr = new byte[1024];
            while (true) {
-               int read = openRawResource.read(bArr);
+               int read = openRawCityResource.read(bArr);
                if (read <= 0) {
                    break;
                }
-               fileOutputStream.write(bArr, 0, read);
+               fileCityOutputStream.write(bArr, 0, read);
            }
 
            //Flushuje O stream, pa zatvara IO streamove
-           fileOutputStream.flush();
-           fileOutputStream.close();
-           openRawResource.close();
+           fileCityOutputStream.flush();
+           fileCityOutputStream.close();
+           openRawCityResource.close();
 
        } catch (IOException e) {
 
@@ -99,8 +98,58 @@ public class PocetnaFragment extends Fragment {
                 Log.e("Error", String.valueOf(e));
             }
         if (dataCity != null){
-            displayData();
+            //displayData();
+            try {
+
+                //Isto kod kao i gore iznad samo se izvršava nad drugom bazom
+                InputStream openRawAttractionResource = getActivity().getAssets().open("rawatrakcije.db");
+                FileOutputStream fileAttractionOutputStream = new FileOutputStream(getActivity().getDatabasePath(ATTRACTION_DATABASE_NAME));
+
+                //Kopira po 1024 byta istovremeno, loopuje sve dok ne prođe kroz sve
+                byte[] bArr = new byte[1024];
+                while (true) {
+                    int read = openRawAttractionResource.read(bArr);
+                    if (read <= 0) {
+                        break;
+                    }
+                    fileAttractionOutputStream.write(bArr, 0, read);
+                }
+
+
+                fileAttractionOutputStream.flush();
+                fileAttractionOutputStream.close();
+                openRawAttractionResource.close();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            try {
+                //Za testiranje
+                dataAttraction = liteAttractionDataHelper.getData();
+                if (dataAttraction != null){
+
+                    new Handler().postDelayed(new Runnable() {
+                        @Override
+                        public void run() {
+                            getActivity().findViewById(R.id.loaderBox).setVisibility(View.GONE);
+                            getActivity().findViewById(R.id.fragmentProgBar).setVisibility(View.VISIBLE);
+                            getActivity().findViewById(R.id.errImg).setVisibility(View.GONE);
+                            getActivity().findViewById(R.id.loaderError).setVisibility(View.GONE);
+
+                            displayData();
+                        }
+                    }, 3000);
+
+
+                }else {
+                    //Error
+                }
+            }catch (Exception e){
+                e.printStackTrace();
+            }
         }else {
+
+            //Error
 
         }
     }
@@ -109,8 +158,15 @@ public class PocetnaFragment extends Fragment {
 
         cityFeatured.setHasFixedSize(true);
         cityFeatured.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
-        featuredAdapter = new FeaturedAdapter(getContext(), dataCity);
-        cityFeatured.setAdapter(featuredAdapter);
+        featuredCityAdapter = new FeaturedCityAdapter(getContext(), dataCity);
+        cityFeatured.setAdapter(featuredCityAdapter);
+
+        attractionFeatured.setHasFixedSize(true);
+        attractionFeatured.setLayoutManager(new LinearLayoutManager(getContext(), LinearLayoutManager.HORIZONTAL, false));
+        attractionAdapter = new FeaturedAttractionAdapter(getContext(), dataAttraction);
+        attractionFeatured.setAdapter(attractionAdapter);
+
+
 
     }
 }
